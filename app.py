4fba3a2e21921d0776ee0212427dc20b94805be1
marialejas-narvaez <marialejas@gmail.com>
2026-05-11 21:05:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1327,6 +1327,6 @@ st.divider()
 st.markdown("""
 <div style="text-align:center;color:#adb5bd;font-size:12px;padding:8px 0;">
     FISULAB · IA de apoyo clínico · Proyecto académico — Datos e Inteligencia Artifical (IA) · 2026<br>
-    <span style="color:#dc3545">Este sistema es experimental. No usar como único criterio dediagnóstico.</span>
+    <span style="color:#dc3545">Este sistema es experimental. No usar como único criterio de diagnóstico.</span>
 </div>
 """, unsafe_allow_html=True)
